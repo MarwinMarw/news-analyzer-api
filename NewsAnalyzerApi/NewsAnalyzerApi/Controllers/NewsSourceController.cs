6bec689d29136ddc104800ce84a033c9fdea5fb5
MILKMAN\wladi <vladislavby96@gmail.com>
2022-10-16 13:04:16 +0100

    add NewsSourceController

---
+++ NewsAnalyzerApi/NewsAnalyzerApi/Controllers/NewsSourceController.cs
@@ -0,0 +1,29 @@
+﻿namespace NewsAnalyzerApi.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class NewsSourceController : Controller
+{
+    private readonly ILogger _logger;
+
+    public NewsSourceController(ILogger<NewsSourceController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpPost]
+    public IActionResult AddSource(Uri newSource)
+    {
+        _logger.LogInformation("Added new news source {NewsSource}", newSource);
+        return Ok(newSource);
+    }
+
+    [HttpDelete]
+    public IActionResult DeleteSource(Uri source)
+    {
+        _logger.LogInformation("Delete news source {NewsSource}", source);
+        return StatusCode(StatusCodes.Status204NoContent);
+    }
+}
